59d10361d864c2ad2c8ae97c712f07c1d3e56bbc
Jacob West <jwest@omnitech-inc.com>
2022-02-21 19:43:55 -0700

    Tweaks to screen layout

--- OdeToBallonsTowerDefence/app/src/main/java/edu/sdsmt/team6/odetoballonstowerdefence/GameActivity.java
+++ OdeToBallonsTowerDefence/app/src/main/java/edu/sdsmt/team6/odetoballonstowerdefence/GameActivity.java
@@ -2,11 +2,9 @@ package edu.sdsmt.team6.odetoballonstowerdefence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +16,7 @@ public class MainActivity extends AppCompatActivity {
             this.getSupportActionBar().hide();
         }
         catch (NullPointerException e){}
-
-        setContentView(R.layout.activity_main);
-
-    }
-
-    public void onGameStart(View view) {
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
+        
+        setContentView(R.layout.activity_game);
     }
 }
