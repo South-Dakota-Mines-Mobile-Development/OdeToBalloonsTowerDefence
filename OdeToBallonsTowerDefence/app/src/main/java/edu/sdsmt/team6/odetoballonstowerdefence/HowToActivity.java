9c8db34666d2d30a76678307c38d518b2fdb0c9e
Jacob West <jwest@omnitech-inc.com>
2022-03-07 17:48:17 -0700

    Updated the how to play

--- OdeToBallonsTowerDefence/app/src/main/java/edu/sdsmt/team6/odetoballonstowerdefence/HowToActivity.java
+++ OdeToBallonsTowerDefence/app/src/main/java/edu/sdsmt/team6/odetoballonstowerdefence/HowToActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class HowToActivity extends AppCompatActivity {
 
